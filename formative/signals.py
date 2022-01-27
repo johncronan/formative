9bb451363a7698916db1957cab1dff3a008e7928
John Kyle Cronan <john@pbx.org>
2022-01-27 13:30:40 -0600

    labels keep up with block name changes

--- formative/signals.py
+++ formative/signals.py
@@ -1,5 +1,6 @@
 from django.db.models import Q, Exists, OuterRef
-from django.db.models.signals import post_save, pre_delete, post_delete
+from django.db.models.signals import pre_save, post_save, \
+    pre_delete, post_delete
 from django.dispatch import receiver
 from django.utils.text import capfirst
 
@@ -25,12 +26,19 @@ def form_pre_delete(sender, instance, **kwargs):
     if form.status != Form.Status.DRAFT:
         form.unpublish()
 
+@receiver(pre_save, sender=CustomBlock)
+def customblock_pre_save(sender, instance, raw, **kwargs):
+    if raw: return
+    
+    if instance.pk:
+        orig = CustomBlock.objects.get(pk=instance.pk)
+        instance._old_name = orig.name
 
 @receiver(post_save, sender=CustomBlock)
 def customblock_post_save(sender, instance, created, raw, **kwargs):
-    if raw or not created: return
+    if raw: return
 
-    block = instance
+    block, new = instance, created
     if not block.page: return # no autocreated labels for autocreated fields
 
     # set up default labels for the block
@@ -46,26 +54,84 @@ def customblock_post_save(sender, instance, created, raw, **kwargs):
     text = capfirst(block.name)
     if style != FormLabel.LabelStyle.WIDGET: text += ':' # TODO: i18n
     
-    l = FormLabel(form=block.form, path=block.name, style=style, text=text)
-    l.save()
+    l = None
+    if new:
+        l = FormLabel(form=block.form, path=block.name, style=style, text=text)
+    elif block.name != block._old_name:
+        try:
+            l = FormLabel.objects.get(form=block.form, path=block._old_name,
+                                      style=style)
+            l.path = block.name
+        except FormLabel.DoesNotExist: pass
+    if l: l.save()
     
-    if block.type == CustomBlock.InputType.CHOICE:
-        for c in block.choices():
-            text = capfirst(c)
-            l = FormLabel(form=block.form, path='.'.join((block.name, c)),
+    if block.type != CustomBlock.InputType.CHOICE: return
+    paths = []
+    for c in block.choices():
+        text = capfirst(c)
+        new_path = '.'.join((block.name, c))
+        if not new and block.name != block._old_name:
+            path = '.'.join((block._old_name, c))
+        else: path=new_path
+        
+        l = None
+        if not new:
+            try:
+                l = FormLabel.objects.get(form=block.form, path=path,
+                                          style=FormLabel.LabelStyle.WIDGET)
+                l.path = new_path
+            except FormLabel.DoesNotExist: pass
+        
+        if not l:
+            l = FormLabel(form=block.form, path=new_path,
                           style=FormLabel.LabelStyle.WIDGET, text=text)
-            l.save()
+        
+        l.save()
+        paths.append(l.path)
+    
+    delete_sub_labels(block.form, block.name, paths)
+    if block._old_name != block.name:
+        delete_sub_labels(block.form, block._old_name)
+
+@receiver(pre_save, sender=FormBlock)
+def formblock_pre_save(sender, instance, raw, **kwargs):
+    if raw: return
+    
+    if instance.pk:
+        orig = FormBlock.objects.get(pk=instance.pk)
+        instance._old_name = orig.name
 
 @receiver(post_save, sender=FormBlock)
 def formblock_post_save(sender, instance, created, raw, **kwargs):
-    if raw or not created: return # TODO: need to check for name change(&others)
+    if raw: return
 
-    block, stock = instance, instance.stock
+    block, stock, new = instance, instance.stock, created
 
     # get stock block's FormLabels and save
-    for path, (style, text) in stock.widget_labels().items():
-        l = FormLabel(form=block.form, path=path, style=style, text=text)
+    paths = []
+    for new_path, (style, text) in stock.widget_labels().items():
+        if not new and block.name != block._old_name:
+            if '.' in new_path:
+                widget = new_path[new_path.index('.')+1:]
+                path = '.'.join((block._old_name, widget))
+            else: path = block._old_name
+        else: path = new_path
+        if not new:
+            try:
+                l = FormLabel.objects.get(form=block.form, path=path,
+                                          style=style)
+                l.path = new_path
+            except FormLabel.DoesNotExist: pass
+        
+        if not l:
+            l = FormLabel(form=block.form, path=new_path, style=style,
+                          text=text)
         l.save()
+        paths.append(l.path)
+    
+    delete_sub_labels(block.form, block.name, paths)
+    if block._old_name != block.name:
+        delete_sub_labels(block.form, block._old_name)
 
 @receiver(post_save, sender=CollectionBlock)
 def collectionblock_post_save(sender, instance, created, raw, **kwargs):
@@ -94,13 +160,17 @@ def collectionblock_post_save(sender, instance, created, raw, **kwargs):
     l = FormLabel.objects.get_or_create(form=block.form, path=block.name,
                                         defaults={'style': style, 'text': text})
     
-    if block.fixed: return
+    if block.fixed: return # no implementation yet for fixed + collection
     style = FormLabel.LabelStyle.WIDGET
     for name in block.collection_fields():
         text, path = capfirst(name), '.'.join((block.name, name))
         l = FormLabel.objects.get_or_create(form=block.form, path=path,
                                             defaults={'style': style,
                                                       'text': text})
+
+def delete_sub_labels(form, name, exclude=[]):
+    sl = form.labels.filter(path__startswith=name+'.').exclude(path__in=exclude)
+    sl.delete()
 
 def delete_block_labels(form, name):
     form.labels.filter(Q(path=name) | Q(path__startswith=name+'.')).delete()
